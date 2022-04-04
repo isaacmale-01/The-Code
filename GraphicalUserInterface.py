bca70ae63b97fa88698136cde864c264bd2c99ae
isaacmale-01 <75686170+isaacmale-01@users.noreply.github.com>
2022-04-04 17:40:41 +0100

    master

---
+++ GraphicalUserInterface.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import tkinter
+
+
+window = Tk()
+window.title("The Sound of The Students")
+window.configure(background="purple")
+
+#insert image in this section - I tried but you can't get the exact file structure to work here?!
+
+window.mainloop()
