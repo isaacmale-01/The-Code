2e902ea0520f2de288974aaa6a378b29ba9d1a61
isaacmale-01 <isaacmale@hotmail.co.uk>
2022-04-04 21:27:31 +0100

    master

--- GraphicalUserInterface.py
+++ GraphicalUserInterface.py
@@ -4,7 +4,21 @@ import tkinter
 
 window = Tk()
 window.title("The Sound of The Students")
+window.geometry("800x600+10+30")
 window.configure(background="purple")
+samplewelcometext = "Welcome to Isaac Male's Third Year Project!"
+
+
+lbl=Label(window, text=samplewelcometext, fg='red', font=("Helvetica", 16))
+lbl.place(x=60, y=50)
+window.title('Hello Python')
+window.geometry("300x200+10+10")
+
+
+
+btn=Button(window, text="Let's Get Started!", fg='blue')
+btn.place(x=350, y=200)
+window.title('Hello Python')
 
 #insert image in this section - I tried but you can't get the exact file structure to work here?!
 
---
+++ GraphicalUserInterfacev2.py
@@ -0,0 +1,66 @@
+from tkinter import *
+import tkinter
+from PIL import Image, ImageTk
+
+class Window(Frame):
+
+    def __init__(self, master=None):
+        Frame.__init__(self, master)               
+        self.master = master
+        self.init_window()
+
+
+    def init_window(self):
+
+        self.master.title("The Sound of The Students")       
+        self.pack(fill=BOTH, expand=1)
+
+        
+        menu = Menu(self.master)
+        self.master.config(menu=menu)        
+        file = Menu(menu)      
+        file.add_command(label="Exit", command=self.client_exit)        
+        menu.add_cascade(label="File", menu=file)        
+        edit = Menu(menu)     
+
+        file.add_command(label="Show Img", command=self.showImg)
+        file.add_command(label="Show Text", command=self.showText)
+
+
+        edit.add_command(label="Undo")       
+        menu.add_cascade(label="Edit", menu=edit)
+
+    def showImg(self):
+        load = Image.open("projectlogoupdate2.jpg")
+        resize_image = load.resize((300,300), Image.ANTIALIAS)
+        img = ImageTk.PhotoImage(resize_image)
+        render = ImageTk.PhotoImage(load)
+
+        # labels can be text or images
+        img = Label(self, image=render)
+        img.image = render
+        img.place(x=40, y=100)
+
+    def showText(self):
+        text = Label(self, text="Hey! Welcome to Isaac Male's Third Year Project!")
+        text.pack()
+
+
+        startButton = Button(self, text="Let's Get Started!")        
+        startButton.place(x=340, y=350)
+        quitButton = Button(self, text="Quit", command=self.client_exit)        
+        quitButton.place(x=370, y=400)
+
+        self.configure(bg='purple')
+
+    def client_exit(self):
+        exit()
+
+
+root = Tk()
+
+root.geometry("800x600")
+
+app = Window(root)   
+
+root.mainloop()
